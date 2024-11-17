e8dba61d355bc23620cd0ff8dfa97d62f8f5a8dd
Otavio Mariano Camargo Duarte <devotaviomarianoduarte@gmail.com>
2024-11-16 22:54:28 -0300

    Update ex106.py

--- MUNDO 3/ex106.py
+++ MUNDO 3/ex106.py
@@ -1,14 +1,24 @@
-def ajuda(com):
-    help(com)
+from time import sleep
+
+def cabecalho(titulo=str, simbolo=str):
+    tam = len(titulo) + 4
+    print(f'{simbolo}' * tam)
+    print(titulo.center(tam))
+    print(f'{simbolo}' * tam)
 
 
+def funcaoBiblioteca(funcao):
+    sleep(1)
+    cabecalho(f'Acessando o manual do comando {funcao}', '=')
+    sleep(1)
+    help(funcao)
 
 
-# Programa Principal
-comando = ''
 while True:
-    comando = str(input('Função ou Biblioteca: '))
-    if comando.upper() == 'FIM':
+    cabecalho('SISTEMA DE AJUDA PyHELP', '=')
+    escolha = str(input('Função ou Biblioteca > '))
+    if escolha.upper() == 'FIM':
         break
     else:
-        ajuda(comando)
+        funcaoBiblioteca(escolha)
+cabecalho('ATÉ LOGO!', '=')
